2960b596214b49ff37559a680e162817f96a413e
Aristidescosta <aristidiscosta200@gmail.com>
2023-11-02 22:05:07 +0100

    feat: adicionando a barra de pesquisa e botão de entrar

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -1,8 +1,26 @@
-import { Box, HStack, Text, useBreakpointValue } from "@chakra-ui/react";
+import {
+	Avatar,
+	Box,
+	Button,
+	Flex,
+	HStack,
+	Input,
+	InputGroup,
+	InputRightElement,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
+	Text,
+	useBreakpointValue,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import React from "react";
 import { TNavbarItem } from "../../types/NavbarItem";
 import NavigationBar from "./NavgationBar";
+import { SearchIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { StorageEnum, getData } from "../../database/LocalStorageDAO";
+import { APP_VARIANT_COLOR } from "../../utils/constants";
 /* import { APP_COLOR } from '../../utils/constants' */
 
 interface IHeader {
@@ -11,20 +29,82 @@ interface IHeader {
 
 export const Header: React.FC<IHeader> = ({ navbar }) => {
 	const isBase = useBreakpointValue({ base: true, md: true, lg: false });
+	const user = getData(StorageEnum.Login);
+	const username = user ? user.split("@")[0] : null;
 	return (
 		<Box>
-			<Box display={'flex'} justifyContent={"space-between"} h={["20", "96px"]} mx={["2", "8"]}>
+			<Box
+				display={"flex"}
+				justifyContent={"space-between"}
+				alignItems={"center"}
+				h={["20", "96px"]}
+				mx={["2", "8"]}
+			>
 				<Box as={Link} display={"flex"} alignSelf={"center"} to={"/"}>
 					<Text as={"h1"} textTransform={"capitalize"}>
 						AnimeLib
 					</Text>
 				</Box>
-				<NavigationBar items={navbar} isBase={isBase || false}/>
+				<NavigationBar items={navbar} isBase={isBase || false} />
+				<HStack display={"flex"} justify={"space-between"}>
+					<InputGroup>
+						<InputRightElement pointerEvents={"none"}>
+							<SearchIcon color={"grey.300"} />
+						</InputRightElement>
+						<Input
+							type="search"
+							placeholder="Pesquise por um anime, mangá ou personagem"
+						/>
+					</InputGroup>
+
+					{user ? (
+						<Menu>
+							<Flex alignItems={"center"} justifyContent={"center"}>
+								<Flex alignItems={"center"} justifyContent={"center"} gap={2}>
+									<Avatar
+										name={username}
+										src="https://github.com/Aristidescosta.png"
+									/>
+									<Text fontSize={"small"}>Aristides Costa</Text>
+								</Flex>
+								<MenuButton
+									as={Button}
+									aria-label="Options"
+									rightIcon={<ChevronDownIcon />}
+									variant="outline"
+									border={"none"}
+								/>
+							</Flex>
+
+							<MenuList>
+								<MenuItem as={"a"} href="/perfil">
+									Perfil
+								</MenuItem>
+								<MenuItem as={"a"} href="/logout">
+									Sair
+								</MenuItem>
+							</MenuList>
+						</Menu>
+					) : (
+						<Button
+							border={`1px solid ${APP_VARIANT_COLOR}`}
+							color={APP_VARIANT_COLOR}
+							borderRadius={"8"}
+							display={["none", "flex"]}
+							alignSelf={"center"}
+							background="transparent"
+							_hover={{
+								bg: "transparent",
+							}}
+							fontSize={["2px, 8px, 19px"]}
+							as={Link}
+							to={"/entrar"}
+						>
+							Entrar
+						</Button>
+					)}
+				</HStack>
 			</Box>
-
-			<HStack>
-
-			</HStack>
 		</Box>
 	);
 };
