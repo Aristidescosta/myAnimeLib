f782c0d88c1b58272c0ad05f3ae8e3728d9f0173
Aristidescosta <aristidiscosta200@gmail.com>
2023-11-05 09:18:12 +0100

    feat: implementado a funcionalidade de assistir trailer

--- src/shared/components/Featured/index.tsx
+++ src/shared/components/Featured/index.tsx
@@ -1,18 +1,25 @@
 import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 import React, { useState, useEffect } from "react";
 import { BsFillPlayFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
 import {
+	AspectRatio,
 	Box,
 	Flex,
 	Highlight,
 	IconButton,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalHeader,
+	ModalOverlay,
 	Text,
+	useDisclosure,
 	useMediaQuery,
 	useTheme,
 } from "@chakra-ui/react";
 
-import { APP_VARIANT_COLOR } from "../../utils/constants";
+import { APP_COLOR, APP_VARIANT_COLOR } from "../../utils/constants";
 import { AnimeData } from "../../types/AnimeData";
 
 interface IFeaturedProps {
@@ -22,6 +29,7 @@ interface IFeaturedProps {
 
 export const Featured: React.FC<IFeaturedProps> = ({ items, type }) => {
 	const theme = useTheme();
+	const { isOpen, onClose, onOpen } = useDisclosure();
 
 	const breakpoints = {
 		sm: theme.breakpoints["sm"],
@@ -34,7 +42,7 @@ export const Featured: React.FC<IFeaturedProps> = ({ items, type }) => {
 	const [item, setItem] = useState<AnimeData>(items[itemPosition]);
 
 	const genres = [];
-	if (item.genres) {
+	if (item) {
 		for (const i in item.genres) {
 			genres.push(item.genres[i].name);
 		}
@@ -68,113 +76,140 @@ export const Featured: React.FC<IFeaturedProps> = ({ items, type }) => {
 		}
 	};
 
+	const handleOpenTrailler = () => {
+		onOpen();
+	};
+
 	return (
-		<Box
-			as="section"
-			bgPos={"center"}
-			bgSize={"cover"}
-			bgImg={item.images.jpg.large_image_url}
-			h={"80vh"}
-			alignSelf={"flex-start"}
-			transition="transform 0.3s"
-			pos={"relative"}
-		>
+		<>
 			<Box
-				bgGradient={"to top, #111 10%, transparent 90%"}
-				w={"inherit"}
-				h={"inherit"}
+				as="section"
+				bgPos={"center"}
+				bgSize={"cover"}
+				bgImg={item.images.jpg.large_image_url}
+				h={"80vh"}
+				alignSelf={"flex-start"}
+				transition="transform 0.3s"
+				pos={"relative"}
 			>
 				<Box
-					style={{
-						background: "linear-gradient(to top, #111 10%, transparent 90%)",
-					}}
+					bgGradient={"to top, #111 10%, transparent 90%"}
 					w={"inherit"}
 					h={"inherit"}
 				>
-					<Flex
-						w={"inherit"}
-						h={"inherit"}
-						flexDir={"column"}
-						pb={150}
-						pl={30}
-						pt={70}
+					<Box
 						style={{
 							background: "linear-gradient(to top, #111 10%, transparent 90%)",
 						}}
+						w={"inherit"}
+						h={"inherit"}
 					>
-						<Text fontSize={isSm[0] ? 24 : 60} fontWeight={"bold"}>
-							{item.title}
-						</Text>
-						<Box fontSize={isSm[0] ? 14 : 18} fontWeight={"bold"} mt={15}>
-							<Text display={"inline-block"} mr={15} color={APP_VARIANT_COLOR}>
-								{type.toUpperCase()}
-							</Text>
-							<Text display={"inline-block"} mr={15}>
-								{item.year}
-							</Text>
-							<Highlight
-								query={genres}
-								styles={{ px: "1", py: "1", bg: "#999" }}
-							>
-								{genres.join(" ")}
-							</Highlight>
-						</Box>
-						<Flex gap={4} alignItems={"center"}>
-							<IconButton
-								isRound={true}
-								variant="solid"
-								colorScheme="teal"
-								aria-label="Done"
-								fontSize="20px"
-								icon={<BsFillPlayFill />}
-								as={Link}
-								to={"/myList"}
-							/>
-							<Text
-								fontSize={isSm[0] ? 9 : 20}
-								color={"#FFFFFF"}
-								maxW={"40%"}
-								mt={15}
-							>
-								{description}
-							</Text>
-						</Flex>
-
 						<Flex
-							alignItems={"center"}
-							color={"#999"}
-							alignSelf={"flex-end"}
-							fontSize={60}
-							pos={"absolute"}
-							bottom={20}
-							right={20}
+							w={"inherit"}
+							h={"inherit"}
+							flexDir={"column"}
+							pb={150}
+							pl={30}
+							pt={70}
+							style={{
+								background:
+									"linear-gradient(to top, #111 10%, transparent 90%)",
+							}}
 						>
-							<IconButton
-								aria-label="Previous anime"
-								fontSize="40px"
-								icon={<ArrowBackIcon />}
-								onClick={handleClickLeftButton}
-								color={"#999"}
-								bg={"none"}
-							/>
-							<Flex>
-								<Text>0{itemPosition + 1}</Text>
-								<Text alignSelf={"flex-end"} mt={-25} fontSize={20}>
-									/04
+							<Text fontSize={isSm[0] ? 24 : 60} fontWeight={"bold"}>
+								{item.title}
+							</Text>
+							<Box fontSize={isSm[0] ? 14 : 18} fontWeight={"bold"} mt={15}>
+								<Text
+									display={"inline-block"}
+									mr={15}
+									color={APP_VARIANT_COLOR}
+								>
+									{type.toUpperCase()}
+								</Text>
+								<Text display={"inline-block"} mr={15}>
+									{item.year}
+								</Text>
+								<Highlight
+									query={genres}
+									styles={{ px: "1", py: "1", bg: "#999" }}
+								>
+									{genres.join(" ")}
+								</Highlight>
+							</Box>
+							<Flex gap={4} alignItems={"center"}>
+								<IconButton
+									isRound={true}
+									variant="solid"
+									colorScheme="teal"
+									aria-label="Done"
+									fontSize="20px"
+									icon={<BsFillPlayFill />}
+									onClick={handleOpenTrailler}
+								/>
+								<Text
+									fontSize={isSm[0] ? 9 : 20}
+									color={"#FFFFFF"}
+									maxW={"40%"}
+									mt={15}
+								>
+									{description}
 								</Text>
 							</Flex>
-							<IconButton
-								aria-label="Previous anime"
-								fontSize="40px"
-								icon={<ArrowForwardIcon />}
-								onClick={handleClickRightButton}
-								color={"#FFF"}
-								bg={"none"}
-							/>
+
+							<Flex
+								alignItems={"center"}
+								color={"#999"}
+								alignSelf={"flex-end"}
+								fontSize={60}
+								pos={"absolute"}
+								bottom={20}
+								right={20}
+							>
+								<IconButton
+									aria-label="Previous anime"
+									fontSize="40px"
+									icon={<ArrowBackIcon />}
+									onClick={handleClickLeftButton}
+									color={"#999"}
+									bg={"none"}
+								/>
+								<Flex>
+									<Text>0{itemPosition + 1}</Text>
+									<Text alignSelf={"flex-end"} mt={-25} fontSize={20}>
+										/04
+									</Text>
+								</Flex>
+								<IconButton
+									aria-label="Previous anime"
+									fontSize="40px"
+									icon={<ArrowForwardIcon />}
+									onClick={handleClickRightButton}
+									color={"#FFF"}
+									bg={"none"}
+								/>
+							</Flex>
 						</Flex>
-					</Flex>
+					</Box>
 				</Box>
 			</Box>
-		</Box>
+
+			<Modal onClose={onClose} isOpen={isOpen}>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader color={APP_COLOR}>{item.title}</ModalHeader>
+					<ModalCloseButton color={APP_COLOR} />
+					<ModalBody bgColor={APP_COLOR} w={"full"} p={0}>
+						<AspectRatio w={"full"} h={"full"} ratio={1}>
+							<iframe
+								title={item.title}
+								src={item.trailer.embed_url}
+								allowFullScreen
+							/>
+						</AspectRatio>
+					</ModalBody>
+				</ModalContent>
+			</Modal>
+		</>
 	);
 };
