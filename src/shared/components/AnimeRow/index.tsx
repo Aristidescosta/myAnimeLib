5bb01b900fddb128d0d0e88d850d134370abd2c8
Aristidescosta <aristidiscosta200@gmail.com>
2023-11-07 19:08:40 +0100

    refactor: controlando a tela

--- src/shared/components/AnimeRow/index.tsx
+++ src/shared/components/AnimeRow/index.tsx
@@ -1,72 +1,104 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
-  Box,
-  Heading,
-  useDisclosure,
-  useMediaQuery,
-  useTheme,
+	Box,
+	Heading,
+	useDisclosure,
+	useMediaQuery,
+	useTheme,
 } from "@chakra-ui/react";
 import "swiper/css";
 
 import { AnimeData } from "../../types/AnimeData";
 import { AnimeCard, ModalAnime } from "..";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y, EffectCards } from "swiper/modules";
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import 'swiper/css/effect-fade';
+import {
+	Navigation,
+	Pagination,
+	Scrollbar,
+	A11y,
+	EffectCards,
+} from "swiper/modules";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/effect-fade";
+import "./style.css";
 
 interface IAnimeRowProps {
-  title: string;
-  items: AnimeData[];
+	title: string;
+	items: AnimeData[];
+}
+
+interface WindowSize {
+	width: number;
+	height: number;
 }
 
 export const AnimeRow: React.FC<IAnimeRowProps> = ({ title, items }) => {
-  const theme = useTheme();
+	const [animeItem, setAnimeItem] = useState<AnimeData | null>(null);
+	const handleClickAnimeCard = (item: AnimeData) => {
+		setAnimeItem(item);
+		onOpen();
+	};
+	const [windowSize, setWindowSize] = useState<WindowSize>({
+		width: window.innerWidth,
+		height: window.innerHeight,
+	});
 
-  const breakpoints = {
-    sm: theme.breakpoints["sm"],
-  };
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			});
+		};
 
-  const isSm = useMediaQuery(`(max-width: ${breakpoints.sm})`);
+		window.addEventListener("resize", handleResize);
 
-  const [animeItem, setAnimeItem] = useState<AnimeData | null>(null);
-  const handleClickAnimeCard = (item: AnimeData) => {
-    setAnimeItem(item);
-    onOpen();
-  };
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
-  const { isOpen, onClose, onOpen } = useDisclosure();
-
-  return (
-    <>
-      <Box mb={30} ml={6}>
-        <Heading as={"h2"} mb={8}>
-          {title}
-        </Heading>
-        <Swiper
-          spaceBetween={50}
-          modules={[Navigation, Pagination, Scrollbar, A11y, EffectCards ]}
-          navigation
-          pagination={{ clickable: true }}
-          scrollbar={{ draggable: true }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
-		  effect="card"
-          slidesPerView={isSm[0] ? 1.5 : 6}
-			style={{ padding: 30 }}
-		>
-          {items.map((item, key) => (
-            <SwiperSlide key={key} style={{ overflow: "visible" }}>
-              <AnimeCard item={item} handleClick={handleClickAnimeCard} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </Box>
-      {animeItem && (
-        <ModalAnime item={animeItem} isOpen={isOpen} onClose={onClose} />
-      )}
-    </>
-  );
+	const { isOpen, onClose, onOpen } = useDisclosure();
+	console.log(windowSize.width);
+	return (
+		<>
+			<Box mb={30} ml={6}>
+				<Heading as={"h2"} mb={8}>
+					{title}
+				</Heading>
+				<Swiper
+					modules={[Navigation, Pagination, Scrollbar, A11y, EffectCards]}
+					navigation
+					pagination={{ clickable: true }}
+					scrollbar={{ draggable: true }}
+					effect="card"
+					slidesPerView={
+						windowSize.width <= 480
+							? 1.5
+							: windowSize.width <= 768
+							? 2
+							: windowSize.width <= 980
+							? 3
+							: windowSize.width <= 10242
+							? 4
+							: windowSize.width <= 1200
+							? 5
+							: 6
+					}
+					style={{ padding: 30 }}
+				>
+					{items.map((item, key) => (
+						<SwiperSlide key={key} style={{ overflow: "visible" }}>
+							<AnimeCard item={item} handleClick={handleClickAnimeCard} />
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</Box>
+			{animeItem && (
+				<ModalAnime item={animeItem} isOpen={isOpen} onClose={onClose} />
+			)}
+		</>
+	);
 };
