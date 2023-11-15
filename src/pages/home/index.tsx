ba37a9c361dfcb847414beb915d7da1df8c4ec1c
Aristidescosta <aristidiscosta200@gmail.com>
2023-11-15 15:50:15 +0100

    refactor: melhorando a requisição dos dados

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,147 +11,170 @@ import { APP_VARIANT_COLOR } from "../../shared/utils/constants";
 import jikanDB from "../../jikanDB";
 import { AnimeData } from "../../shared/types/AnimeData";
 
+export interface IAnimeListProps {
+  slug: string;
+  title: string;
+  items: any;
+}
+
 export const Home: React.FC = () => {
-	const {
-		calculateIntervalBetweenDates,
-		setAnimeData,
-		setAnimeList,
-		animeList,
-		animeData,
-	} = useDataAnime();
-	const [isLoading, setIsLoading] = useState(true);
+  const {
+    calculateIntervalBetweenDates,
+    setAnimeData,
+    setAnimeList,
+    animeList,
+    animeData,
+  } = useDataAnime();
+  const [isLoading, setIsLoading] = useState(true);
 
-	const { ToastStatus } = useToastMessage();
-	const { isOnline, setIsOnline } = useVerifyInternet();
-	const toast = useToast();
-	const toastIdRef = useRef<number | undefined>();
+  const { ToastStatus } = useToastMessage();
+  const { isOnline, setIsOnline } = useVerifyInternet();
+  const toast = useToast();
+  const toastIdRef = useRef<number | undefined>();
 
-	useEffect(() => {
-		online().then((online) => {
-			setIsOnline(online);
-		});
+  useEffect(() => {
+    online().then((online) => {
+      setIsOnline(online);
+    });
 
-		const handleOnlineStatusChange = () => {
-			setIsOnline(navigator.onLine);
-		};
+    const handleOnlineStatusChange = () => {
+      setIsOnline(navigator.onLine);
+    };
 
-		window.addEventListener("online", handleOnlineStatusChange);
-		window.addEventListener("offline", handleOnlineStatusChange);
+    window.addEventListener("online", handleOnlineStatusChange);
+    window.addEventListener("offline", handleOnlineStatusChange);
 
-		return () => {
-			window.removeEventListener("online", handleOnlineStatusChange);
-			window.removeEventListener("offline", handleOnlineStatusChange);
-		};
-	}, []);
+    return () => {
+      window.removeEventListener("online", handleOnlineStatusChange);
+      window.removeEventListener("offline", handleOnlineStatusChange);
+    };
+  }, []);
 
-	useEffect(() => {
-		if (!isOnline) {
-			toastIdRef.current = toast({
-				title: "Internet",
-				description: "Conexão a internet perdida",
-				status: ToastStatus.WARNING,
-				duration: 900000,
-				position: "top-right",
-			}) as number;
-		} else {
-			if (toastIdRef.current) {
-				toast.update(toastIdRef.current, {
-					description: "Conexão restabelecida",
-					status: ToastStatus.INFO,
-					duration: 1000,
-				});
-			}
-		}
-	}, [isOnline]);
+  useEffect(() => {
+    if (!isOnline) {
+      toastIdRef.current = toast({
+        description: "Conexão a internet perdida",
+        status: ToastStatus.WARNING,
+        duration: 900000,
+        position: "top-right",
+      }) as number;
+    } else {
+      if (toastIdRef.current) {
+        toast.update(toastIdRef.current, {
+          description: "Conexão restabelecida",
+          status: ToastStatus.INFO,
+          duration: 1000,
+        });
+      }
+    }
+  }, [isOnline]);
 
-	const addDataOnAnimeData = (
-		response: {
-			slug: string;
-			title: string;
-			items: any;
-		}[]
-	) => {
-		let animeDataCount = 0;
-		const ANIME_DATA: AnimeData[] = []
-		while (animeDataCount < 4) {
-			const RANDOM_CHOISE_SLUGS = Math.floor(Math.random() * response.length);
+  const addDataOnAnimeData = (response: IAnimeListProps[]) => {
+    console.log("isto aqui está retornando isto: ", response);
+    let animeDataCount = 0;
+    const ANIME_DATA: AnimeData[] = [];
+    while (animeDataCount < 4) {
+      const RANDOM_CHOISE_SLUGS = Math.floor(Math.random() * response.length);
 
-			const RANDOM_CHOISE = Math.floor(
-				Math.random() * response[RANDOM_CHOISE_SLUGS].items.data.length
-			);
+      const RANDOM_CHOISE = Math.floor(
+        Math.random() * response[RANDOM_CHOISE_SLUGS].items.data.length
+      );
 
-			const choice = response[RANDOM_CHOISE_SLUGS].items.data[RANDOM_CHOISE];
-			if (!animeData.includes(choice)) {
-				animeDataCount++;
-				ANIME_DATA.push(choice)
-			}
-		}
-		setAnimeData(ANIME_DATA)
-	};
+      const choice = response[RANDOM_CHOISE_SLUGS].items.data[RANDOM_CHOISE];
+      if (!animeData.includes(choice)) {
+        animeDataCount++;
+        ANIME_DATA.push(choice);
+      }
+    }
+    setAnimeData(ANIME_DATA);
+  };
 
-	const getAnimeList = () => {
-		jikanDB
-			.getAnimeList()
-			.then((response) => {
-				setAnimeList(response);
-				setIsLoading(false);
-			})
-			.catch(() => {
-				setIsLoading(false);
-			});
-	};
-	useEffect(() => {
-		setIsLoading(true);
-		if (animeList.length === 0 || animeData.length === 0) {
-			getAnimeList();
-		} else {
-			const CURRENT_DATE = new Date();
-			const INTERVAL_BETWEEN_DATES =
-				calculateIntervalBetweenDates(CURRENT_DATE);
-			if (INTERVAL_BETWEEN_DATES && INTERVAL_BETWEEN_DATES.DAYS >= 1) {
-				getAnimeList();
-			} else {
-				setIsLoading(false);
-			}
-		}
+  function getAnimeList(): Promise<IAnimeListProps[]> {
+    return new Promise((resolve, reject) => {
+      jikanDB
+        .getAnimeList()
+        .then((response) => {
+          resolve(response);
+        })
+        .catch(() => {
+          reject({ message: "Ouve um erro interno" });
+        })
+        .finally(() => setIsLoading(false));
+    });
+  }
+
+  useEffect(() => {
+    setIsLoading(true);
+    if (animeList.length === 0 || animeData.length === 0) {
+      getAnimeList()
+        .then((response) => {
+          setAnimeList(response);
+        })
+        .catch((error) => {
+          toast({
+            description: error.message,
+            status: ToastStatus.ERROR,
+            position: "top-right",
+          });
+        });
+    } else {
+      const CURRENT_DATE = new Date();
+      const INTERVAL_BETWEEN_DATES =
+        calculateIntervalBetweenDates(CURRENT_DATE);
+      if (INTERVAL_BETWEEN_DATES && INTERVAL_BETWEEN_DATES.DAYS >= 1) {
+        getAnimeList()
+          .then((response) => {
+            setAnimeList(response);
+          })
+          .catch((error) => {
+            toast({
+              description: error.message,
+              status: ToastStatus.ERROR,
+              position: "top-right",
+            });
+          });
+      } else {
+        setIsLoading(false);
 		addDataOnAnimeData(animeList);
-	}, []);
-	return (
-		<>
-			{isLoading ? (
-				<Box
-					display={"flex"}
-					h={"100%"}
-					w={"100%"}
-					alignItems={"center"}
-					justifyContent={"center"}
-				>
-					<CircularProgress
-						value={30}
-						size={"120px"}
-						color={APP_VARIANT_COLOR}
-						isIndeterminate
-					/>
-				</Box>
-			) : animeData.length > 0 ? (
-				<>
-					<Featured items={animeData} />
-					<Box>
-						<Box as="section">
-							{animeList.length > 0 &&
-								animeList.map((item, key) => (
-									<AnimeRow
-										key={key}
-										title={item.title}
-										items={item.items.data}
-									/>
-								))}
-						</Box>
-					</Box>
-				</>
-			) : (
-				<EmptyMessage message="Tivemos um erro interno, por favor recarrege a página!" />
-			)}
-		</>
-	);
+      }
+    }
+  }, []);
+  return (
+    <>
+      {isLoading ? (
+        <Box
+          display={"flex"}
+          h={"100%"}
+          w={"100%"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <CircularProgress
+            value={30}
+            size={"120px"}
+            color={APP_VARIANT_COLOR}
+            isIndeterminate
+          />
+        </Box>
+      ) : animeData.length > 0 ? (
+        <>
+          <Featured items={animeData} />
+          <Box>
+            <Box as="section">
+              {animeList.length > 0 &&
+                animeList.map((item, key) => (
+                  <AnimeRow
+                    key={key}
+                    title={item.title}
+                    items={item.items.data}
+                  />
+                ))}
+            </Box>
+          </Box>
+        </>
+      ) : (
+        <EmptyMessage message="Tivemos um erro interno, por favor recarrege a página!" />
+      )}
+    </>
+  );
 };
