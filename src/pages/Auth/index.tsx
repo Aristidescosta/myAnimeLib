debe26deafa33df67f5c438cc5f01fccc9677045
Aristidescosta <aristidiscosta200@gmail.com>
2023-11-19 19:37:44 +0100

    refactor: ajustando a tela de login

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -4,14 +4,14 @@ import { AuthScreen } from "./AuthScreen";
 
 export const Auth = () => {
 	return (
-		<Box h={"full"} w={"full"} pos={"relative"}>
+		<Box h={"100vh"} w={"100vw"} pos={"relative"}>
 			<Box
 				bgPos={"center"}
 				bgSize={"cover"}
 				bgRepeat={"no-repeat"}
 				bgImg={"/banner.jpg"}
-				h={"100vh"}
-				w={"100vw"}
+				h={"100%"}
+				w={"100%"}
 				filter={"blur(4px)"}
 			/>
 			<AuthScreen />
