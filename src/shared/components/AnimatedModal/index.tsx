b290feaad2ba0a6abd60a0a11afdecc7675559e8
Aristidescosta200 <aristidescosta330@gmail.com>
2023-11-30 23:16:40 +0100

    refactor: apresentando o X

--- src/shared/components/AnimatedModal/index.tsx
+++ src/shared/components/AnimatedModal/index.tsx
@@ -7,10 +7,12 @@ import {
 	ModalBody,
 	Button,
 	Text,
+	Center,
 } from "@chakra-ui/react";
 import React from "react";
 import { motion } from "framer-motion";
 import { APP_COLOR } from "../../utils/constants";
+import "./animatedStyle.css";
 
 interface IAnimatedModalProps {
 	isOpen: boolean;
@@ -26,17 +28,26 @@ export const AnimatedModal: React.FC<IAnimatedModalProps> = ({
 			<Modal isOpen={isOpen} onClose={onClose}>
 				<ModalOverlay />
 
-				<ModalContent>
+				<ModalContent color={APP_COLOR}>
 					<ModalHeader>Modal Animado</ModalHeader>
-					<ModalBody>
-						<motion.div
-							initial={{ opacity: 0, translateY: 50 }}
-							animate={{ opacity: 1, translateY: 0 }}
-							exit={{ opacity: 0, translateY: 50 }}
-							transition={{ duration: 0.3 }}
-						>
-							<Text bgColor={APP_COLOR}></Text>
-						</motion.div>
+					<ModalBody mt={2}>
+						<Center flexDir={"column"}>
+							<motion.div
+								className="x-container"
+								initial={{ scale: 0 }}
+								animate={{ scale: 1 }}
+								transition={{
+									type: "spring",
+									stiffness: 260,
+									damping: 20,
+								}}
+							>
+								<motion.div className="x-line" />
+								<motion.div className="x-line" />
+							</motion.div>
+							<Text>Oops...</Text>
+							<Text>Você precisa estar logado para marca essa opção!</Text>
+						</Center>
 					</ModalBody>
 					<ModalFooter>
 						<Button colorScheme="blue" mr={3} onClick={onClose}>
