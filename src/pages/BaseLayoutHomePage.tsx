90910c1dd4db564cd251e0e261f9f9b72eda8e2a
Aristidescosta200 <aristidescosta330@gmail.com>
2023-12-02 14:13:03 +0100

    refactor: alteração para deploy

--- src/pages/BaseLayoutHomePage.tsx
+++ src/pages/BaseLayoutHomePage.tsx
@@ -1,28 +1,10 @@
 import { PropsWithChildren } from 'react'
 import { Box } from '@chakra-ui/react'
 
-import { TNavbarItem } from '../shared/types/NavbarItem'
 import { Header } from '../shared/components/Header'
 
 export const BaseLayoutHomePage = (props: PropsWithChildren) => {
-	const NAVBAR_ITEMS: TNavbarItem[] = [
-		{
-			link: '/',
-			menu: 'Início'
-		},
-		{
-			link: '/animes',
-			menu: 'Animes'
-		},
-		{
-			link: '/mangas',
-			menu: 'Mangás'
-		},
-		{
-			link: '/personagens',
-			menu: 'Personagens'
-		}
-	]
+	
 
 	return (
 		<Box>
