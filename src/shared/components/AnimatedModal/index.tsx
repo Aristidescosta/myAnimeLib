f8c05f961d12f106086cf80825e46a2df764ad9b
Aristidescosta200 <aristidescosta330@gmail.com>
2023-12-02 13:59:14 +0100

    refactor: configurado o modal

--- src/shared/components/AnimatedModal/index.tsx
+++ src/shared/components/AnimatedModal/index.tsx
@@ -25,38 +25,41 @@ export const AnimatedModal: React.FC<IAnimatedModalProps> = ({
 }) => {
 	return (
 		<>
-			<Modal isOpen={isOpen} onClose={onClose}>
-				<ModalOverlay />
+			<Center>
+				<Modal isOpen={isOpen} onClose={onClose}>
+					<ModalOverlay />
 
-				<ModalContent color={APP_COLOR}>
-					<ModalHeader>Modal Animado</ModalHeader>
-					<ModalBody mt={2}>
-						<Center flexDir={"column"}>
-							<motion.div
-								className="x-container"
-								initial={{ scale: 0 }}
-								animate={{ scale: 1 }}
-								transition={{
-									type: "spring",
-									stiffness: 260,
-									damping: 20,
-								}}
-							>
-								<motion.div className="x-line" />
-								<motion.div className="x-line" />
-							</motion.div>
-							<Text>Oops...</Text>
-							<Text>Você precisa estar logado para marca essa opção!</Text>
-						</Center>
-					</ModalBody>
-					<ModalFooter>
-						<Button colorScheme="blue" mr={3} onClick={onClose}>
-							Fechar
-						</Button>
-						<Button variant="ghost">Ação Secundária</Button>
-					</ModalFooter>
-				</ModalContent>
-			</Modal>
+					<ModalContent color={APP_COLOR} p={4}>
+						<ModalBody mt={2}>
+							<Center flexDir={"column"}>
+								<motion.div
+									className="x-container"
+									initial={{ scale: 0 }}
+									animate={{ scale: 1 }}
+									transition={{
+										type: "spring",
+										stiffness: 260,
+										damping: 20,
+									}}
+								>
+									<motion.div className="x-line" />
+									<motion.div className="x-line" />
+								</motion.div>
+								<Text>Oops...</Text>
+								<Text>Você precisa estar logado para marca essa opção!</Text>
+							</Center>
+						</ModalBody>
+						<ModalFooter>
+							<Button colorScheme="blackAlpha" mr={3} onClick={onClose}>
+								Fechar
+							</Button>
+							<Button variant="ghost" colorScheme="messenger">
+								Fazer login
+							</Button>
+						</ModalFooter>
+					</ModalContent>
+				</Modal>
+			</Center>
 		</>
 	);
 };
